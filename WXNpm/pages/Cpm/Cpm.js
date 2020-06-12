ac34accac32b88d2fae897757e7aad2ae7d2fc70
hnbwyh <rainwang@hinabian.com>
2020-06-12 19:16:36 +0800

    信息完善。

--- WXNpm/pages/Cpm/Cpm.js
+++ WXNpm/pages/Cpm/Cpm.js
@@ -1,19 +1,22 @@
-// pages/Home/Home.js
+// pages/Cpm/Cpm.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    inputShowed: false,
+    inputVal: "",
+    focus:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-    
+    this.setData({
+      search:this.search.bind(this)
+    })
   },
 
   /**
@@ -27,10 +30,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    // const etc = this.getOpenerEventChannel()
-    // etc.on("abcdef",function(info){
-    //   console.log("信息流:",info,";参数:",info.data);
-    // })
 
   },
 
@@ -52,8 +51,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    console.log(" onPullDownRefresh ");
-    
+
   },
 
   /**
@@ -70,27 +68,26 @@ Page({
 
   },
 
-  clickButtonEvent:function (e){
-    console.log(e.currentTarget.dataset.value)
-    wx.navigateTo({
-      url: '/pages/Detail/Detail?info=8866',
-      success:function(res){
-        res.eventChannel.emit("aptdata",{data:e.currentTarget.dataset.value}) 
-      }
-
-    })
-  },
-
-  callpicker:function (e){
-    wx.navigateTo({
-      url: '/pages/Picker/Picker',
-    })
-  },
-
-  clickevent: function(e){
-    // 自定义组件触发事件时提供的detail对象
-    console.log("onClickevent :",e)
+  search:function (e){
+    console.log(" search :",e);
     
-  }
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve([{text: '搜索结果', value: 1}, {text: '搜索结果2', value: 2}, 
+                   {text: '搜索结果3', value: 3}, {text: '搜索结果4', value: 4}, 
+                   {text: '搜索结果5', value: 5}, {text: '搜索结果6', value: 6}])
+          //resolve([])
+      }, 200)
+  })
+    
+  },
+
+  selectResult:function (e){
+    console.log("selectResult :",e);
+    
+    wx.navigateTo({
+      url: '/pages/index/index',
+    })
+  },
 
 })
--- WXTest/Component/CardCell/CardCell.js
+++ WXTest/Component/CardCell/CardCell.js
@@ -1,5 +1,35 @@
 // Component/CardCell/CardCell.js
 Component({
+
+  /**
+   * 组件的生命周期
+   */
+  lifetimes:{
+    created:function (){console.log("lifetimes-created");},
+
+    attached:function () {console.log("lifetimes-attached");},
+
+    ready:function (){console.log("lifetimes-ready");},
+
+    moved:function(){console.log("lifetimes-moved");},
+
+    detached:function () {console.log("lifetimes-detached");},
+
+    error:function(){console.log("lifetimes-error");},
+  },
+
+  /**
+   * 组件所在页面的生命周期
+   */
+  pageLifetimes:{
+    show:function(){console.log("pageLifetimes-show");
+    },
+    hide:function(){console.log("pageLifetimes-hide");
+    },
+    resize:function(){console.log("pageLifetimes-resize");
+    },
+  },
+
   /**
    * 组件的属性列表
    */
@@ -21,16 +51,15 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    customMethod:function(e){
-      console.log("customMethod:",e)
-      
+  
+    onTap: function(){
+      var myEventDetail = {detail:"detail"} // detail对象，提供给事件监听函数
+      var myEventOption = {option:"option"} // 触发事件的选项
+      this.triggerEvent('clickevent', myEventDetail, myEventOption)
     }
+
   },
 
-  testevent:function (e) {
-    console.log("testevent:",e);
-    
-    this.methods.customMethod("testdata")
-  },
+
 
 })
