f20ebd61267f7a5fb30688d3f12cef92cff22867
hnbwyh <rainwang@hinabian.com>
2020-07-30 18:53:41 +0800

    完善。

--- Water/WaterShop/Common_js/api.js
+++ Water/WaterShop/Common_js/api.js
@@ -1,7 +1,7 @@
 var URL = {}
 URL.vtfcode     = "https://www.ifreewater.com/users/vcode"
 URL.vtflogin    = "https://www.movead.xyz/user/login"
-URL.uploadiage  = "https://www.ifreewater.com/upload/image"
+URL.uploadiage  = "https://www.ifreewater.com/uploads/image"
 URL.addshop     = "https://www.ifreewater.com/shops"
 URL.addproduct  = "https://www.ifreewater.com/products"
 
--- Water/WaterShop/pages/Vertify/Vertify.js
+++ Water/WaterShop/pages/Vertify/Vertify.js
@@ -17,72 +17,10 @@ Page({
     tels:{title:"商家电话：",holderTxt:"请输入",type:"tels"},
     
     // 有图片
-    shopdoor:{title:"店铺门面照片",remark:"照片需包含完整的牌匾与门槛",eximg:"",epl:"查看样例",type:"shopdoor"},
-    shopinner:{title:"店铺内部照片",remark:"照片需真实反应店铺内部环境",eximg:"",epl:"查看样例",type:"shopinner"},
-    // shopperfront:{title:"身份证正面照",remark:"照片需真实反应店铺内部环境",eximg:"",epl:"查看样例",type:"shopperfront"},
-    // shopperback:{title:"身份证反面照",remark:"照片需真实反应店铺内部环境",eximg:"",epl:"查看样例",type:"shopperback"},
-    // shopperidf:{title:"手持身份证照片",remark:"照片需真实反应店铺内部环境",eximg:"",epl:"查看样例",type:"shopperidf"},
     shoplicense:{title:"营业执照",remark:"照片需真实反应店铺内部环境",eximg:"",epl:"查看样例",type:"shoplicense"},
     shoppermit:{title:"许可证",remark:"照片需真实反应店铺内部环境",eximg:"",epl:"查看样例",type:"shoppermit"},
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-      
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
 
   valueforinput:function (e){
     let t = e.currentTarget.dataset.type
@@ -119,93 +57,48 @@ Page({
 
   },
 
-  tapevent:function (e) {
-    console.log(e)
-
-    wx.chooseImage({
-      count:1,
-      success(res){
-        //tempFilePath可以作为img标签的src属性显示图片
-        var tmp = res.tempFilePaths
-
-      },
-    })
-
-  },
-
   chooseTheImage:function(e) {
-    var tmp = ""
+    let t = e.currentTarget.dataset.tp
+    var localPath = ""
     wx.chooseImage({
-
       success(res){
-        tmp = res.tempFilePaths[0]
+        localPath = res.tempFilePaths[0]
         // 网络图片上传
         wx.uploadFile({
-          filePath: tmp,
-          name: 'shopimage',
+          filePath: localPath,
+          name: 'name',
           url: api.URL.uploadiage,
           success(res){
-            console.log("图片上传-success：",res)
-          },
-          fail(res){
+            if (t == "shoplicense"){
+              console.log("营业执照图片上传：",res)
+            }else if (t == "shoppermit"){
+              console.log("许可证图片上传：",res)
+            }
+          },fail(res){
             console.log("图片上传-fail：",res)
-          },
-          complete(res) {
-            console.log("图片上传-complete：",res)
+          },complete(res) {
+            
           }
         })
 
-      },
-
-      complete: (res) => {
-        if (tmp.length > 0){
-          let t = e.currentTarget.dataset.tp
+      },complete: (res) => {
+        if (localPath.length > 0){
           var info = {}
-          if (t == "shopdoor") {
-            info = this.data.shopdoor
-            info["eximg"] = tmp;
-            this.setData({
-              shopdoor:info
-            })
-          }else if (t == "shopinner"){
-            info = this.data.shopinner
-            info["eximg"] = tmp;
-            this.setData({
-              shopinner:info
-            })
-          }else if (t == "shopperfront"){
-            info = this.data.shopperfront
-            info["eximg"] = tmp;
-            this.setData({
-              shopperfront:info
-            })
-          }else if (t == "shopperback"){
-            info = this.data.shopperback
-            info["eximg"] = tmp;
-            this.setData({
-              shopperback:info
-            })
-          }else if (t == "shopperidf"){
-            info = this.data.shopperidf
-            info["eximg"] = tmp;
-            this.setData({
-              shopperidf:info
-            })
-          }else if (t == "shoplicense"){
+          if (t == "shoplicense"){
             info = this.data.shoplicense
-            info["eximg"] = tmp;
+            info["eximg"] = localPath;
             this.setData({
               shoplicense:info
             })
           }else if (t == "shoppermit"){
             info = this.data.shoppermit
-            info["eximg"] = tmp;
+            info["eximg"] = localPath;
             this.setData({
               shoppermit:info
           })
         }
         
-        console.log("chooseImage:",t,"   info:",info,"测试结果：",tmp.indexOf('wxfile')>=0)
+        console.log("chooseImage:",t,"   info:",info,"测试结果：",localPath.indexOf('wxfile')>=0)
 
         }
       },
@@ -215,17 +108,7 @@ Page({
 
   lookexample:function(e) {
     let t = e.currentTarget.dataset.tp
-    if (t == "shopdoor") {
-      
-    }else if (t == "shopinner"){
-
-    }else if (t == "shopperfront"){
-
-    }else if (t == "shopperback"){
-
-    }else if (t == "shopperidf"){
-
-    }else if (t == "shoplicense"){
+    if (t == "shoplicense"){
 
     }else if (t == "shoppermit"){
 
